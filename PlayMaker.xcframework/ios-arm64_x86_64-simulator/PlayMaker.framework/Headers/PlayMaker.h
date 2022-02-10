53fdf47d382bd1cbb3a6374cbb24cdcb74d35b73
Julius Kiano <jgkiano@gmail.com>
2022-02-10 20:43:10 +0300

    initial commit

---
+++ PlayMaker.xcframework/ios-arm64_x86_64-simulator/PlayMaker.framework/Headers/PlayMaker.h
@@ -0,0 +1,18 @@
+//
+//  PlayMaker.h
+//  PlayMaker
+//
+//  Created by Julius Kiano on 10/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayMaker.
+FOUNDATION_EXPORT double PlayMakerVersionNumber;
+
+//! Project version string for PlayMaker.
+FOUNDATION_EXPORT const unsigned char PlayMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayMaker/PublicHeader.h>
+
+
